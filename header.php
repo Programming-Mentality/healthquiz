3a7642655eb6bad4ba82ef060b1668a03a4216eb
Programming-Mentality <98261228+Programming-Mentality@users.noreply.github.com>
2022-03-23 17:34:33 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,48 @@
+<!doctype html>
+    <html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/magnific-popup.css">
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css1.css">
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <!-- <link rel="stylesheet" href="css/owl.theme.default.min.css"> -->
+        <link rel="icon" href="icon.png" type="image/x-icon">
+    </head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a href="index.php" class="navbar-brand">You Will <img src="icon.png" alt="" width="30" height="24" class="d-inline-block align-text-top"> Again</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="index.php" class="smoothScroll ">Home</a></li>
+                        <li class="dropdown nav-item">
+                            <a href="#about" class="dropbtn">Awareness</a>
+                            <div class="dropdown-content">
+                                <a href="depression.html">Depression</a>
+                                <a href="kindyourself.html">Caring For Yourself </a>
+                            </div>
+                        </li>
+                        <li class="dropdown nav-item">
+                            <a href="#team" class="dropbtn">Expert Profile</a>
+                            <div class="dropdown-content">
+                                <a href="#">Campigns</a>
+                                <a href="#">NGOs</a>
+                            </div>
+                        </li>
+                        <li class="nav-item"><a href="#news" class="smoothScroll">Motivation</a></li>
+                        <li class="nav-item"><a href="#appointment1" class="smoothScroll">Contact</a></li>
+                        <li class="appointment-btn nav-item">
+                            <a class="btn btn-primary" href="#main" role="button" style="background-color: #a5c422; border-color: #a5c422; margin: 0.5rem;">Make an appointment</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
